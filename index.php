f027e5811a5e8126310db3af472454394868eac0
Harrisonnganga <ngangaharrison490@gmail.com>
2024-05-27 23:43:35 +0300

    updated signup form

--- index.php
+++ index.php
@@ -47,41 +47,7 @@
                                 </div>
                             </form>
                             <div class="py-4 text-center">
-                                Join now, <a href="signup.html" class="link">Sign Up</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <!-- sign-up -->
-    <section class="main-section">
-        <div class="container">
-            <div class="justify-content-center align-items-center">
-                <div class="col-md-7 col-lg-4">
-                    <div class="box rounded">
-                        <div>
-                            <div class="login-box p-5">
-                                <h2 class="pb-4">Sign Up</h2>
-                                <form action="" method="post">
-                                    <div class="mb-4">
-                                        <input type="text" class="form-control" placeholder="Enter Name" name="name" required>
-                                      </div>
-                                      <div class="mb-4">
-                                        <input type="email" class="form-control" placeholder="Enter Email address" name="email" required>
-                                      </div>
-                                      <div class="mb-4">
-                                        <input type="password" class="form-control" placeholder="Enter Password" name="password" required>
-                                      </div>
-                                      <div class="d-grid gap-2">
-                                        <button type="submit" class="btn btn-primary-1" name="signup">Sign Up</button>
-                                      </div>
-                                </form>
-                                <div class="py-4 text-center">
-                                    <a href="login.html" class="link">Login</a>
-                                  </div>
+                                Join now, <a href="signup.php" class="link">Sign Up</a>
                             </div>
                         </div>
                     </div>
