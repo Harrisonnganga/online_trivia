b9bca2f4283315ff2bbc164f715bb74ba84507fa
Harrisonnganga <ngangaharrison490@gmail.com>
2024-05-28 07:26:40 +0300

    update

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php
-require_once "connect.php";
+require_once "Database.php";
 require_once "function.php";
 session_start();
 if (isset($_SESSION['login_active'])) {
-  header("Location: dashboard.php");
-  exit();
+    header("Location: dashboard.php");
+    exit();
 } else {
-  if (isset($_POST['login'])) {
-    $email = santize($_POST['email']);
-    $inputpassword = santize($_POST['password']);
-    $password = md5($inputpassword);
-    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
-    $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) > 0) {
-      while ($row = mysqli_fetch_assoc($result)) {
-        $_SESSION['login_active'] = [$row["name"], $row["email"]];
-        $_SESSION['msg'] = "Welcome to Dashboard";
-        $_SESSION['class'] = "text-bg-success";
-        header("Location: dashboard.php");
-        exit();
-      }
-    } else {
-      $_SESSION['msg'] = "Check Email & Password";
-      $_SESSION['class'] = "text-bg-danger";
-      header("Location: index.php");
-      exit();
+    if (isset($_POST['login'])) {
+        $email = santize($_POST['email']);
+        $inputpassword = santize($_POST['password']);
+        $password = md5($inputpassword);
+        $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $_SESSION['login_active'] = [$row["name"], $row["email"]];
+                $_SESSION['msg'] = "Welcome to Dashboard";
+                $_SESSION['class'] = "text-bg-success";
+                header("Location: dashboard.php");
+                exit();
+            }
+        } else {
+            $_SESSION['msg'] = "Check Email & Password";
+            $_SESSION['class'] = "text-bg-danger";
+            header("Location: index.php");
+            exit();
+        }
     }
-  }
 }
 ?>
 <!DOCTYPE html>
@@ -42,17 +42,23 @@ if (isset($_SESSION['login_active'])) {
 <body>
     <section class="main-section">
         <div class="container">
+        <?php if (isset($_SESSION['msg'])) : ?>
             <div class="toast-container position-fixed bottom-0 end-0 p-3">
                 <div class="toast fade show" role="alert" aria-live="assertive" aria-atomic="true">
                     <div class="toast-header">
                         <strong class="me-auto">Message</strong>
-                        <button type="button" class="btn-close text-white" data-bs-dismiss="toast"
-                            aria-label="Close">
+                        <button type="button" class="btn-close text-white" data-bs-dismiss="toast" aria-label="Close">
                         </button>
                     </div>
                     <div class="toast-body"></div>
+                    <?php
+                    $message = $_SESSION['msg'];
+                    unset($_SESSION['msg']);
+                    echo $message;
+                    ?>
                 </div>
             </div>
+            <?php endif; ?>
             <div class="row justify-content-center align-items-center" style="height: 100vh">
                 <div class="col-md-7 col-lg-4">
                     <div class="box rounded">
