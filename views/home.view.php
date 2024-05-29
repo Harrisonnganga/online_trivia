81e4c25e21908187d5ad4886546ce9f21d92353d
Harrisonnganga <ngangaharrison490@gmail.com>
2024-05-29 23:30:33 +0300

    home

---
+++ views/home.view.php
@@ -0,0 +1,20 @@
+<?php include "partials/head.php"; ?>
+
+<body>
+    <div class="container">
+        <div class="row justify-content-center align-items-center" style="height: 100vh">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <h1 class="card-title">Welcome to Online Trivia!</h1>
+                        <p class="card-text">Test your knowledge and see how well you score!</p>
+                        <a href="/login" class="btn btn-primary m-2">Login</a>
+                        <a href="/signup" class="btn btn-secondary m-2">Sign Up</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
